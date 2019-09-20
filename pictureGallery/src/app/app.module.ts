7280f0919873ebae8068937b0077e19a827cc932
Abfackeln <synart21@gmail.com>
2019-09-20 23:24:25 +0300

    add routing

--- pictureGallery/src/app/app.module.ts
+++ pictureGallery/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
