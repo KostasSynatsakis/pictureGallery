13f1aae87be4c77c910ab3739f334d4b2c0ce2c2
Abfackeln <synart21@gmail.com>
2019-09-21 00:15:37 +0300

    create http api service

--- pictureGallery/src/app/gallery/gallery.component.ts
+++ pictureGallery/src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line: ban-types
+  images: Object;
+  
+  // tslint:disable-next-line: variable-name
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
+    this._http.getPhotos().subscribe(data => {
+      this.images = data;
+      console.log(this.images);
+    });
   }
 
 }
