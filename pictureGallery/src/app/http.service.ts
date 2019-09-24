64b21bb346cb3bc9958092b1d04510088017ddd2
Abfackeln <synart21@gmail.com>
2019-09-24 17:57:07 +0300

    update image api

--- pictureGallery/src/app/http.service.ts
+++ pictureGallery/src/app/http.service.ts
@@ -9,6 +9,6 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   getPhotos() {
-    return this.http.get('https://api.openbrewerydb.org/breweries');
+    return this.http.get('https://picsum.photos/v2/list');
   }
 }
